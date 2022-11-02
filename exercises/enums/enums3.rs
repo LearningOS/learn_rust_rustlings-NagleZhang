9aae7307e139b6720e8598af20f9d2d27707defb
nagle.zhang <nagle.zhang@sap.com>
2022-11-02 16:14:37 +0800

    to hashmap.

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,13 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point) ,
+    Quit,
+
 }
 
 struct Point {
@@ -37,6 +40,18 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        /*
+        ChangeColor((u8, u8, u8)),
+        Echo(String),
+        Move(Point) ,
+        Quit,
+         */
+        match message {
+            Message::ChangeColor(s) => self.change_color(s),
+            Message::Echo(e) => self.echo(e),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
